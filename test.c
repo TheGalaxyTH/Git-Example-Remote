f8978e0098fc6bdfb32db07ec659cee5f2e39725
12777 <1277740415@qq.com>
2022-09-20 17:01:52 +0800

    Create test.c which print Hello world

---
+++ test.c
@@ -0,0 +1,15 @@
+//
+//  Created by th on 2022/9/20
+//
+#include<stdio.h>
+void printOut(){
+    //edit your code here
+    printf("Hello World");
+}
+int main(){
+
+    //call the printOut function
+    printOut();
+
+    return 0;
+}
